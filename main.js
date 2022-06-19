cb069bd45abb411cc4dc2ed176ec4b9802088072
DanielMalede <danielmalede@gmail.com>
2022-06-19 18:11:38 +0300

    .

--- main.js
+++ main.js
@@ -27,10 +27,47 @@
 //     console.log(iterator);
 // }
 
-function changeColor() {
-    let sameSameButDifferent = document.getElementsByClassName("sameSameButDifferent")
-    for (const iterator of sameSameButDifferent) {
-        iterator.style="color:blue"
-        iterator.innerText = "hello"
-    }
+// function changeColor() {
+//     let sameSameButDifferent = document.getElementsByClassName("sameSameButDifferent")
+//     for (const iterator of sameSameButDifferent) {
+//         iterator.style="color:blue"
+//         iterator.innerText = "hello"
+//     }
+// }
+
+// let h3_line = document.getElementById("h3_line")
+// let newObject = {
+//     myName:"daniel",
+//     myLastName:"malede",
+//     myPhoneNumber:"052-473-5905",
+//     myAddress:"Ben Gavriel",
+// }
+// for (let key in newObject) {
+//     h3_line.innerText += `${key}:${newObject[key]}`+'\n'
+// }
+let userInput1 = document.createElement("input")
+document.body.append(userInput1)
+let BTN = document.createElement("button")
+document.body.append(BTN)
+BTN.innerText= "clack me"
+let h1_line = document.createElement("h1")
+document.body.append(h1_line)
+let newObject = {
+    myName:"",
+    myLastName:"",
+    myPhoneNumber:"",
+    myAddress:"",}
+let array = []
+for (let key in newObject) {
+    array.push(key);
+}   
+let counter = 0;
+BTN.onclick=()=>{
+    newObject[array[counter]] = userInput1.value;
+        h1_line.innerText+= `${array[counter++]} : ${newObject[array[counter]] } \n`
+        console.log(newObject);
 }
+
+   
+
+
